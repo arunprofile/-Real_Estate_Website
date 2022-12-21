2b45f3b34749296347695ec32b6c72fc69eb4506
Arun <aruntp4u@gmail.com>
2022-12-21 15:46:59 +0530

    secret key changed

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bbp8&svw@o4d#h*l!hi*q%_mhuf8ntj2qfs75@@g4e3q$yszkf'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
